16d21513b2b9f72bee288e7f708de86b0035ca0a
Isaac Kelly <isaacmkelly0@gmail.com>
2019-06-26 22:10:05 -0500

    Successfully deleted initialized frames, trying to make container fixed size, no success yet

--- script.js
+++ script.js
@@ -25,5 +25,11 @@ function set(x){
 }
 set(25);//call to set initial size of grid
 reset.addEventListener('click', function(){
-  
+  //loop to erase the grid
+  let child = container.lastElementChild
+  while(child){
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+  set(16);//reset grid to initial size
 });
