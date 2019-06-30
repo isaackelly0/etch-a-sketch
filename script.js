c3df1954065e0443ec1e691b45e991b705d3c624
Isaac Kelly <isaacmkelly0@gmail.com>
2019-06-29 19:32:35 -0500

    added random color generator and a reset button

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 const body = document.querySelector('center');
 const container = document.createElement('div');
 const reset = document.querySelector('#reset');
+const shuffle = document.querySelector('#randomize');
+const solid = document.querySelector('#solid');
 container.classList.add('container');
 body.appendChild(container);
 var gridSize = 16;
 var colorChange = 'sketch';
-function set(x){
+function set(size, color){
   //create grid rows
-  for(let i = 0; i < x; i++){
+  for(let i = 0; i < size; i++){
     let grid = document.createElement('div');
     grid.classList.add('grid');
     grid.setAttribute('id', '' + i);
     //create grid cells
-      for(let j = 0; j < x; j++){
+      for(let j = 0; j < size; j++){
         let frame = document.createElement('div');
         //add class and size attributes
         frame.classList.add('frame');
-        frame.style.height = (960 / x) + "px";
-        frame.style.width = (960 / x) + "px";
+        frame.style.height = (960 / size) + "px";
+        frame.style.width = (960 / size) + "px";
         //create hover function to change color
-        frame.addEventListener('mouseover', function(){
+        if(color === 'sketch'){
+          frame.addEventListener('mouseover', function(){
           frame.classList.add('sketch')
-        })
-        grid.style.marginBottom = -1 * (x) + "px";
+          })
+        }
+        else{
+          frame.addEventListener('mouseover', function(){
+            let numbers = randomize();
+            frame.style.backgroundColor = 'rgb(' + numbers[0] + ',' + numbers[1] + ',' + numbers[2] +')';
+          })
+        }
+        grid.style.marginBottom = -1 * (size) + "px";
         grid.appendChild(frame);
       }
     container.appendChild(grid);
   }
 }
-set(40)
+set(gridSize, colorChange);
 
 reset.addEventListener('click', function(){
   //loop to erase the grid
@@ -55,6 +65,31 @@ reset.addEventListener('click', function(){
   }
 
 });
+//fetch random rgb colors
 function randomize(){
-
+  let rgb = [];
+  for(let i = 0; i < 3; i++){
+    rgb.push(Math.floor(Math.random() * 255));
+  }
+  return rgb;
 }
+//change back to black and white sketch pad
+solid.addEventListener('click', function(){
+  colorChange = 'sketch';
+  let child =container.lastElementChild;
+  while(child){
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+  set(gridSize, colorChange);
+})
+//change to random color sketch pad
+shuffle.addEventListener('click', function(){
+  let child = container.lastElementChild
+  while(child){
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+  colorChange = 'random';
+  set(gridSize, colorChange);
+})
